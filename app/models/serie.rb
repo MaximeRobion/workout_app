b98a78761ed2891046039d2d4daea32d8fa527ce
Maxime Robion <max.robion@hey.com>
2023-07-19 10:14:25 +0200

    forgot model validation

--- app/models/serie.rb
+++ app/models/serie.rb
@@ -6,6 +6,7 @@ class Serie < ApplicationRecord
   validates :weight, presence: true
   validates :repetition, presence: true
   validates :is_total_weight, inclusion: [true, false]
+  validates :units, presence: true
 
   def total_weight
     if is_total_weight then weight else weight*2+exercise.movement_baseline_weight end
