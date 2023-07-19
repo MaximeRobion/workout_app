b7d51849e46638ca2b3748741a28257fe4540df3
Maxime Robion <max.robion@hey.com>
2023-07-19 10:22:57 +0200

    Create and update with unit

--- app/models/serie.rb
+++ app/models/serie.rb
@@ -1,12 +1,12 @@
 class Serie < ApplicationRecord
   belongs_to :exercise
 
-  enum units: [:kg, :lbs]
+  enum unit: [:kg, :lbs]
 
   validates :weight, presence: true
   validates :repetition, presence: true
   validates :is_total_weight, inclusion: [true, false]
-  validates :units, presence: true
+  validates :unit, presence: true
 
   def total_weight
     if is_total_weight then weight else weight*2+exercise.movement_baseline_weight end
